14e4a303a12396f7de623419679a1d63910f9912
nielley <126012911+nielley@users.noreply.github.com>
2023-02-21 10:24:15 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let option1button=document.querySelector(".option-one");
+let optiononescreen=document.querySelector(".option-one-screen");
+
+
+
+
+
+
+
+option1button.onclick=function(){
+ optiononescreen.style.display="block";
+};
+
+
+
+
+
+
+
